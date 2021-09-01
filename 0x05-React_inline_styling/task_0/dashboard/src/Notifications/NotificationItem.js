33dffa7d101dbc87cfb26c970abde356de1ce382
earamosb8 <earamosb8@gmail.com>
2021-09-01 16:40:44 -0500

    add task =

---
+++ 0x05-React_inline_styling/task_0/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class NotificationItem extends React.PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    const { id, type, html, value, markAsRead } = this.props;
+    let item;
+    value
+      ? item = (
+        <li
+          data-notification-type={ type }
+          onClick={ () => markAsRead(id) }
+        >
+          {value}
+        </li>
+      ) : item = (
+        <li
+          data-notification-type={ type }
+          dangerouslySetInnerHTML={ html }
+          onClick={ () => markAsRead(id) }
+        />
+      );
+    return item;
+  }
+}
+
+NotificationItem.propTypes = {
+  type: PropTypes.string,
+  html: PropTypes.shape({
+    __html: PropTypes.string
+  }),
+  value: PropTypes.string,
+  markAsRead: PropTypes.func
+};
+
+NotificationItem.defaultProps = {
+  type: 'default',
+  html: {},
+  value: '',
+  markAsRead: () => {}
+};
+
+export default NotificationItem;
